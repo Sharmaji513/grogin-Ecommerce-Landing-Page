ff9a4087af31066f960e3aefab1792477e67e053
aditya bhojane <adityabhojane66@gmail.com>
2024-02-09 17:59:25 +0530

    add all files and pages

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        Home2: resolve(__dirname, './src/Home 2 page/Home2.html'),
+      },
+    },
+  },
+})
